a116640295100e5165c3fd86ea8fa54437af856d
Raikouxyz <thailh0902@gmail.com>
2025-08-10 13:56:57 +0700

    up

--- TrainMe.Data/AppDbContext.cs
+++ TrainMe.Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using TrainMe.Core.Entities;
 
 namespace TrainMe.Data
 {
-    internal class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }

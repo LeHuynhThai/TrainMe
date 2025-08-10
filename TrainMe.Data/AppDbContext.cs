9e7d5e81862c003976bd248f960d7fe94df54f3e
Raikouxyz <thailh0902@gmail.com>
2025-08-10 11:19:18 +0700

    up

---
+++ TrainMe.Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainMe.Core.Entities;
+
+namespace TrainMe.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}

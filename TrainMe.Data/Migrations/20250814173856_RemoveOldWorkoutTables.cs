7a4048c0a760c9cc087fa04bb30bcf3919e602b4
Raikouxyz <thailh0902@gmail.com>
2025-08-15 00:43:11 +0700

    up

---
+++ TrainMe.Data/Migrations/20250814173856_RemoveOldWorkoutTables.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainMe.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOldWorkoutTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Drop tables in correct order (child tables first due to foreign key constraints)
+            migrationBuilder.DropTable(name: "ExerciseLogs");
+            migrationBuilder.DropTable(name: "WorkoutExercises");
+            migrationBuilder.DropTable(name: "WorkoutLogs");
+            migrationBuilder.DropTable(name: "Exercises");
+            migrationBuilder.DropTable(name: "Workouts");
+            migrationBuilder.DropTable(name: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Note: This Down method is intentionally left empty
+            // as recreating the old tables would require complex schema recreation
+            // If you need to rollback, restore from backup or manually recreate tables
+        }
+    }
+}

d333a3d826f32bdfe04394303fa077f69e7a6eed
Raikouxyz <thailh0902@gmail.com>
2025-08-15 09:58:25 +0700

    up

---
+++ TrainMe.Core/Mapping/MappingProfile.cs
@@ -0,0 +1,61 @@
+using AutoMapper;
+using TrainMe.Core.DTOs;
+using TrainMe.Core.Entities;
+
+namespace TrainMe.Core.Mapping;
+
+/// <summary>
+/// AutoMapper profile for all entity mappings
+/// </summary>
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        ConfigureUserMappings();
+        ConfigureWorkoutItemMappings();
+    }
+
+    private void ConfigureUserMappings()
+    {
+        // User to DTOs
+        CreateMap<User, UserDto>()
+            .ForMember(dest => dest.WorkoutItems, opt => opt.MapFrom(src => src.WorkoutItems));
+
+        CreateMap<User, UserSummaryDto>();
+
+        // Request DTOs to User
+        CreateMap<CreateUserRequest, User>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.WorkoutItems, opt => opt.Ignore());
+
+        CreateMap<UpdateUserRequest, User>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.WorkoutItems, opt => opt.Ignore());
+    }
+
+    private void ConfigureWorkoutItemMappings()
+    {
+        // WorkoutItem to DTOs
+        CreateMap<WorkoutItem, WorkoutItemDto>();
+        CreateMap<WorkoutItem, WorkoutItemSummaryDto>();
+
+        // Request DTOs to WorkoutItem
+        CreateMap<CreateWorkoutItemRequest, WorkoutItem>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.UserId, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.User, opt => opt.Ignore());
+
+        CreateMap<UpdateWorkoutItemRequest, WorkoutItem>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.UserId, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.User, opt => opt.Ignore());
+    }
+}
